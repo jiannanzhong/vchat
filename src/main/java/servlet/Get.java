d2be37e0089817634ed679864aa92b3d05199aef
zhong <zjn@ayjiayou.com>
2019-06-06 15:42:00 +0800

    保持长连接时检查用户是否掉线

--- src/main/java/servlet/Get.java
+++ src/main/java/servlet/Get.java
@@ -59,7 +59,7 @@ public class Get extends HttpServlet {
             message = "fail";
         } else {
             String sign = Chat.renewSign(id2, uid);
-            while (AppSettings.APP_RUNNING && (chatList = Chat.getChatByUid(id2, uid)).size() == 0) {
+            while (AppSettings.APP_RUNNING && User.verifyUser(uid, uuid) && (chatList = Chat.getChatByUid(id2, uid)).size() == 0) {
                 try {
                     Thread.sleep(800);
                 } catch (Exception e) {
